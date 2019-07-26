7f6a85cbc3e83789eb38ea883706c0109714783a
Sujit More <sujitmore21@gmail.com>
2019-07-26 13:08:17 +0530

    SpringBootMVC:create demo project using HTML

---
+++ src/main/java/com/sujitmo/springmvc/controller/MyController.java
@@ -0,0 +1,24 @@
+package com.sujitmo.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyController {
+
+    @GetMapping(value = "/index")
+    public String indexPage(@RequestParam(name="name",required = false,
+    defaultValue = "World") String name, Model model){
+        model.addAttribute("name",name);
+        return "index";
+    }
+
+    @GetMapping(value="/registration")
+    public String regPage(){
+        return "register";
+    }
+
+}
+
